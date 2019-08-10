6137209cbc53b55b9260417381e0ace5d9209e2e
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-10 17:03:54 +0200

    Fixed homepage social icons

--- src/app/website/root/website-root.module.ts
+++ src/app/website/root/website-root.module.ts
@@ -20,39 +20,44 @@ import { WebsiteNavComponent } from './components/website-nav/website-nav.compon
 import { WebsiteNavItemComponent } from './components/website-nav-item/website-nav-item.component';
 import { WebsiteClipTextComponent } from './components/website-clip-text/website-clip-text.component';
 
-
 export const ROUTES: Routes = [
-    {
-        path: '',
-        component: WebsiteRootComponent,
-        children: [
-            { path: '', loadChildren: '../home/home.module#HomeModule' },
-            { path: 'cv', loadChildren: '../cv/cv.module#CVModule' },
-            { path: 'portfolio', loadChildren: '../portfolio/portfolio.module#PortfolioModule' },
-            { path: 'contact', loadChildren: '../contact/contact.module#ContactModule' }
-        ]
-    }
+  {
+    path: '',
+    component: WebsiteRootComponent,
+    children: [
+      { path: '', loadChildren: '../home/home.module#HomeModule' },
+      { path: 'cv', loadChildren: '../cv/cv.module#CVModule' },
+      {
+        path: 'projects',
+        loadChildren: '../portfolio/portfolio.module#PortfolioModule'
+      },
+      {
+        path: 'contact',
+        loadChildren: '../contact/contact.module#ContactModule'
+      }
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule.forChild(ROUTES),
-        NgbCollapseModule,
-        FontAwesomeModule
-    ],
-    declarations: [
-        WebsiteRootComponent,
-        WebsiteHeaderComponent,
-        WebsiteFooterComponent,
-        WebsiteNavComponent,
-        WebsiteNavItemComponent,
-        WebsiteClipTextComponent
-    ],
-    exports: [ ]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    NgbCollapseModule,
+    FontAwesomeModule
+  ],
+  declarations: [
+    WebsiteRootComponent,
+    WebsiteHeaderComponent,
+    WebsiteFooterComponent,
+    WebsiteNavComponent,
+    WebsiteNavItemComponent,
+    WebsiteClipTextComponent
+  ],
+  exports: []
 })
 export class WebsiteRootModule {
-    constructor() {
-        library.add(faBars);
-    }
+  constructor() {
+    library.add(faBars);
+  }
 }
