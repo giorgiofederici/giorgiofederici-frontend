5de96033b38f7e1b45aecb2580040d5c4a703fe6
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-10 16:01:33 +0200

    Changed backend port in env vars

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,7 +1,7 @@
 export const baseEnv = {
   production: false,
 
-  backendUrl: 'http://localhost:5000',
+  backendUrl: 'http://localhost:3000',
   // UIsUrl: 'http://localhost:4200',
 
   domain: 'localhost'
